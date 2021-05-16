9f3681acce1978232d7d665fd27a1ded922880f9
fboltz <frederic.boltz@gmail.com>
2021-05-16 14:41:57 +0200

    Set environement to allow test with github actions

--- main_test.go
+++ main_test.go
@@ -7,11 +7,19 @@ import (
 	"github.com/jetstack/cert-manager/test/acme/dns"
 )
 
-var (
-	zone = os.Getenv("TEST_ZONE_NAME")
-)
-
 func TestRunsSuite(t *testing.T) {
+	var zone string
+	var manifest string
+	var found bool
+
+	if zone, found = os.LookupEnv("TEST_ZONE_NAME"); found == false {
+		zone = "example.com"
+	}
+
+	if manifest, found = os.LookupEnv("TEST_MANIFEST_PATH"); found == false {
+		manifest = "testdata/godaddy"
+	}
+
 	// The manifest path should contain a file named config.json that is a
 	// snippet of valid configuration that should be included on the
 	// ChallengeRequest passed as part of the test cases.
@@ -19,7 +27,7 @@ func TestRunsSuite(t *testing.T) {
 	fixture := dns.NewFixture(&godaddyDNSProviderSolver{},
 		dns.SetResolvedZone(zone),
 		dns.SetAllowAmbientCredentials(false),
-		dns.SetManifestPath("testdata/godaddy"),
+		dns.SetManifestPath(manifest),
 		dns.SetBinariesPath("__main__/hack/bin"),
 	)
 
