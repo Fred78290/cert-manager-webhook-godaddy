f2be37cc440038e7162e63332bfae1838aa8553c
fboltz <frederic.boltz@gmail.com>
2022-05-08 01:31:34 +0200

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+
 helm install godaddy-webhook \
     --set groupName=aldunelabs.com \
     --set image.repository=devregistry.aldunelabs.com/cert-manager-godaddy \
-    --set image.tag=v1.20.5 \
+    --set image.tag=v1.24.0 \
     --set image.pullPolicy=Always \
     --namespace cert-manager ./deploy/godaddy-webhook $@
+
+kubectl apply -f ./release/aldunelabs.yml
+kubectl apply -f ./release/certificat.yml
